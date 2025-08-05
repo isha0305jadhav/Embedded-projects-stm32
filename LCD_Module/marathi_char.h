80c77bab2e6ba6c5b1755b84cd3243d538e4d610
isha0305jadhav <isha0305jadhav@gmail.com>
2025-08-05 15:29:50 -0700

    Add files via upload

---
+++ LCD_Module/marathi_char.h
@@ -0,0 +1,18 @@
+#ifndef MARATHI_CHARS_H
+#define MARATHI_CHARS_H
+
+#include <stdint.h>
+
+// Define character bitmaps
+extern uint8_t diamond[8];
+extern uint8_t heart[8];
+extern uint8_t marathi_ma[8];
+extern uint8_t marathi_e[8];
+extern uint8_t marathi_sha[8];
+extern uint8_t smiley[8];
+extern uint8_t sad[8];
+extern uint8_t bunny[8];
+extern uint8_t flower[8];
+// Add more as needed
+
+#endif
